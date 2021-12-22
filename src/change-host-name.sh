717ebb1e1827778a1fa47d4633d06b7abe6548d5
KhoaVo <Voanhkhoa18p@gmail.com>
2021-12-22 08:46:49 +0700

    update change-host-name

--- src/change-host-name.sh
+++ src/change-host-name.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "CHANGE PC NAME (HOST NAME) : START"
-echo "Type new name:"
-read -p "Enter New Host Name: " x
+#echo "Type new name:"
+read -p "Enter New Host Name (Ctrl + C to Cancel): " x
 sudo hostnamectl set-hostname $x
+echo "Host Infor:"
 hostnamectl
 
 echo "CHANGE PC NAME (HOST NAME) : DONE"
