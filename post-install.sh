118140464203e91a41bcd8e26b7796d75d80d5db
tuNQws <150335179+tuNQwsISoft@users.noreply.github.com>
2024-01-19 14:11:34 +0700

    Update post-install.sh
    add install Docker & Docker Compose

--- post-install.sh
+++ post-install.sh
@@ -227,6 +227,33 @@ else
 				# 	echo "Installing Gulp"
 				# 	npm install gulp -g
 				# 	;;
+				28)
+					echo "Remove any conflict Docker packages:"
+					for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+					echo "Add Docker's official GPG key:"
+					sudo apt-get update
+					sudo apt-get install ca-certificates curl gnupg
+					sudo install -m 0755 -d /etc/apt/keyrings
+					curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+					sudo chmod a+r /etc/apt/keyrings/docker.gpg
+					echo "Add the repository to Apt sources:"
+					echo \
+						"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+						$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+						sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+					sudo apt-get update
+					echo "Install the Docker packages:"
+					sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+					echo "Verify that Docker Engine installation is successful:"
+					sudo docker run hello-world
+					;;
+				28)
+					echo "Installing Docker Compose"
+					sudo apt-get update
+ 					sudo apt-get install docker-compose-plugin
+					echo "Verify Docker Compose"
+					docker compose version
+					;;
 	    esac
 	done
 fi
